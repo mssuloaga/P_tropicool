52ac2b82e5be482b80f6258baae733e3cd944a43
dmendeznova <54755718+dmendeznova@users.noreply.github.com>
2022-06-10 01:38:14 -0400

    nn
    nn

--- resources/views/trabajadores/index.blade.php
+++ resources/views/trabajadores/index.blade.php
@@ -10,7 +10,7 @@
                         <div style="display: flex; justify-content: space-between; align-items: center;">
 
                             <span id="card_title">
-                                {{ __('Trabajadore') }}
+                                {{ __('Trabajador') }}
                             </span>
 
                              <div class="float-right">                             
