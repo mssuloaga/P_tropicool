e7fe9fb68d07bf29e53abac59388c550f67f9620
MatiPino23 <mpino@ing.ucsc.cl>
2022-06-12 01:33:30 -0400

    Tablas ventas

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,9 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->engine = 'InnoDB';
+            $table->engine="InnoDB";
             $table->bigIncrements('id');
+            $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
