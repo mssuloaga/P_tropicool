63eb6eccb580513bc1c82cd6460d757e306f082e
MatiPino23 <mpino@ing.ucsc.cl>
2022-06-14 12:17:02 -0400

    footer

--- resources/views/layouts/footers/auth.blade.php
+++ resources/views/layouts/footers/auth.blade.php
@@ -2,34 +2,19 @@
   <div class="container-fluid">
     <nav class="float-left">
       <ul>
-        <li>
-          <a href="https://www.creative-tim.com">
-              {{ __('Creative Tim') }}
-          </a>
-        </li>
-        <li>
+        
+        {{-- <li>
           <a href="https://creative-tim.com/presentation">
-              {{ __('About Us') }}
+              {{ __('SOBRE TROPICOOL') }}
           </a>
         </li>
         <li>
           <a href="http://blog.creative-tim.com">
-              {{ __('Blog') }}
+              {{ __('SÍGUENOS') }}
           </a>
-        </li>
-        <li>
-          <a href="https://www.creative-tim.com/license">
-              {{ __('Licenses') }}
-          </a>
-        </li>
+        </li> --}}
+      
       </ul>
     </nav>
-    <div class="copyright float-right">
-      &copy;
-      <script>
-        document.write(new Date().getFullYear())
-      </script>, made with <i class="material-icons">favorite</i> by
-      <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a>
-    </div>
   </div>
 </footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -243,15 +243,13 @@
                 <div class="row">
                     <div class="col-lg-6 h-100 text-center text-lg-start my-auto">
                         <ul class="list-inline mb-2">
-                            <li class="list-inline-item"><a href="#!">About</a></li>
+                            <li class="list-inline-item"><a href="#!">Sobre Tropicool</a></li>
                             <li class="list-inline-item">⋅</li>
-                            <li class="list-inline-item"><a href="#!">Contact</a></li>
+                            <li class="list-inline-item"><a href="#!">Términos y condiciones</a></li>
                             <li class="list-inline-item">⋅</li>
-                            <li class="list-inline-item"><a href="#!">Terms of Use</a></li>
-                            <li class="list-inline-item">⋅</li>
-                            <li class="list-inline-item"><a href="#!">Privacy Policy</a></li>
+                            <li class="list-inline-item"><a href="#!">Política de privacidad</a></li>
                         </ul>
-                        <p class="text-muted small mb-4 mb-lg-0">&copy; Your Website 2022. All Rights Reserved.</p>
+                        <p class="text-muted small mb-4 mb-lg-0">&copy;2022 TROPICOOL Todos los derechos reservados</p>
                     </div>
                     <div class="col-lg-6 h-100 text-center text-lg-end my-auto">
                         <ul class="list-inline mb-0">
