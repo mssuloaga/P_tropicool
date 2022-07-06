60711a167b8eed33ba2e0405d4f3ac70340bd029
cmonttf <cfierro@ing.ucsc.cl>
2022-07-06 03:45:26 -0400

    Actualizacion imagen

--- resources/views/articulos/index.blade.php
+++ resources/views/articulos/index.blade.php
@@ -113,15 +113,21 @@
        </header>
 
        <div class="container" style="margin-top: 8rem;">
-        <div class="row">
+        <div class="row" style="margin-top:1rem;">
             @foreach($articulos as $a)
                 <div class="col-md-3">
                     <div class="card" style="width: 16rem;">
                         <div class="card-body">
-                            <img src="img\cargando.jpg" class="card-img-top" alt="$a->nombre">
+                            <img src="{{ asset('uploads/productos/'.$a->imagen) }}" class="card-img-top" title="{{ $a->descripcion }}" alt="{{ $a->descripcion }}">
                             <h5 class="card-title">{{ $a->nombre }}</h5>
-                            <p class="card-text">${{ $a->precio }}</p>
-                            <a href="#" class="btn btn-primary">Añadir</a>
+                            <div class="row">
+                                <div class="col">
+                                    <p class="card-text">${{ $a->precio }}</p>
+                                </div>
+                                <div class="col">
+                                    <a href="#" class="btn btn-primary">Añadir</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
