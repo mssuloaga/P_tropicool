81ccdeb5252cf27a81f8efef9ab5a9131ebd77c3
dmendeznova <54755718+dmendeznova@users.noreply.github.com>
2022-07-18 17:26:44 -0400

    cal
    cal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,9 +12,16 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
+    
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.0/main.css">
+    <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.0/main.min.js"></script> 
+    <script src="https://cdn.jsdelivr.net/npm/fullcalendar@5.11.0/locales-all.js"></script>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -71,6 +78,8 @@
                 </div>
             </div>
         </nav>
+
+        <script src="{{ asset('js/agenda.js') }}" defer></script>
 
         <main class="py-4">
             @yield('content')
