b7f36c0d297e439272a9d50be124ca5d897c861e
mssuloaga <103213960+mssuloaga@users.noreply.github.com>
2022-07-18 16:13:37 -0400

    diseño gráficos

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,14 +5,14 @@
     <div class="container-fluid">
         <div class="row">
             
-            <div class="col-lg-5 col-sm-12 p-4 rounded overflow-hidden" >
+            <div class="col-lg-5 col-sm-12 p-4 rounded border overflow-hidden" >
             <h2 class="p-2 font-weight-bold  ">Cantidad de productos</h2>
                 <div style="position: relative; height: 400px; width: 400px;">
                     <canvas id="myChart" height= "400" width= "400"></canvas>
                 </div>
             </div>
             <div class="col-1 p-2"></div>
-            <div class="col-lg-5 col-sm-12 p-4  rounded overflow-hidden">
+            <div class="col-lg-5 col-sm-12 p-4  rounded border overflow-hidden">
             <h2 class="p-2 font-weight-bold  ">Precio de los productos</h2>
                 <div style="position: relative; height: 400px; width: 400px;">
                     <canvas id="myBar" height= "400" width= "400"></canvas>
@@ -42,8 +42,8 @@
                     datasets: [{
                         label:'',
                         data:cData.data,
-                        backgroundColor: ['#005B9A','#0191C8','#74C2E1','#8C8984'],
-                        borderColor: ['black'],
+                        backgroundColor: [' #57e2ac ',' #f16b7b','#0ca0b5 ','#846694',' #d87e90','#f8cd5a'],
+                        borderColor: ['yellow'],
                         borderWidth:1
                     }]
                 },
@@ -76,8 +76,8 @@
                     datasets: [{
                         label:'',
                         data:cData2.data2,
-                        backgroundColor: ['#9561e2','#6574cd'],
-                        borderColor: ['black'],
+                        backgroundColor: [' #6bc2e3 ',' #0ca0b5 '],
+                        borderColor: ['yellow'],
                         borderWidth:1
                     }]
                 },
