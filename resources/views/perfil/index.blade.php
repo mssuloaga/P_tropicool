0349ca84bc8e8d989da667e7f1d8c4f032d03cc7
mssuloaga <103213960+mssuloaga@users.noreply.github.com>
2022-07-18 17:17:08 -0400

    botones perfil

--- resources/views/perfil/index.blade.php
+++ resources/views/perfil/index.blade.php
@@ -2,16 +2,18 @@
 
 @section('content')
 
-        <div class="container rounded bg-white mt-5 mb-5">
+<div class="content">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="card">
             <form action="{{route('changePassword')}}" method="POST" class="needs-validation" novalidate>
-            @csrf  
-
-                
+            @csrf                  
                 <div class="row">
                     <div class="col-md-3 border-right">
                         <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"><span class="font-weight-bold">{{ Auth::user()->name }}</span><span class="text-black-50">{{ Auth::user()->email }}</span><span> </span></div>
                     </div>
-                    <div class="col-md-5 border-right">
+                    <div class="col-md-5 border-right overflow-hidden">
                         <div class="p-3 py-5">
                             <div class="d-flex justify-content-between align-items-center mb-3">
                                 <h4 class="text-right">Actualizar perfil</h4>
@@ -69,13 +71,21 @@
                                     </ul>
                                 </div>
                             @endif
-                            <div class="mt-5 text-center"><button type="submit" class="btn btn-primary profile-button" type="button">Guardar Perfil</button></div>
-                            <div class="mt-2 text-center"><a href="{{ route('home') }}" class="btn btn-success">Volver</a></div>
-                        </div>
+                            <div>
+                                 <div class="text-center p-4">
+                                    <button type="submit" class="btn btn-primary">Guardar</button>
+                                    <a href="{{ route('home') }}" class="btn btn-success ms-3"> Volver </a>                    
+                                </div>                   
+                        </div>                        
+                    </div>
                     </div>
                 </div>
             </form>
         </div>
+    </div>
+    </div>
+    </div>
+    </div>
     </div>
 </div>
 @endsection
