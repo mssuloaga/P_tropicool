e99fb35d47f0178974f03bb693738480f62ebe95
cmonttf <cfierro@ing.ucsc.cl>
2022-07-18 20:12:43 -0400

    update

--- app/Http/Controllers/NosotrosController.php
+++ app/Http/Controllers/NosotrosController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Empresa;
 
 class NosotrosController extends Controller
 {
-    public function index(){
-        return view('nosotros.index', compact('nosotros'));
+    public function index()
+    {
+        $nosotros = Empresa::all();
+        return view('nosotros.index', [
+            'nosotros'=>$nosotros
+        ]);
     }
 }
