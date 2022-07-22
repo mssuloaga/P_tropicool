83fee6b897fd8a9239c56c91b5346b7703ae1ab3
cmonttf <cfierro@ing.ucsc.cl>
2022-07-22 01:01:13 -0400

    Update show.blade.php

--- resources/views/articulos/show.blade.php
+++ resources/views/articulos/show.blade.php
@@ -3,7 +3,7 @@
     <section class="content container-fluid">
         <div class="container align-center" style="text-align: center;">
             <center>
-                <div class="card  p-5 mt-3">
+                <div class="card  p-3">
                     <img src="{{ asset('uploads/productos/'.$articulos->imagen) }}" class="card-img-top" alt="">
                     <div class="card-body">
                         <h5 class="card-title">{{ $articulos->nombre }}</h5>
