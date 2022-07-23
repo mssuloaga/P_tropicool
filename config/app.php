bf3842d1e9f05a1e9245c4810de518bc01e1f222
NicolasAburto <naburtoa@ing.ucsc.cl>
2022-07-23 14:58:41 -0400

    codigo qr

--- config/app.php
+++ config/app.php
@@ -180,6 +180,7 @@ return [
         App\Providers\FortifyServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
         Maatwebsite\Excel\ExcelServiceProvider::class,
+        SimpleSoftwareIO\QrCode\QrCodeServiceProvider::class,
     ],
 
     /*
@@ -233,6 +234,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'PDF' => \Barryvdh\DomPDF\Facade::class,
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+        'QrCode' => SimpleSoftwareIO\QrCode\Facades\QrCode::class,
     ],
 
 ];
