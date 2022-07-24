7b91093ed8b9c3935c470c0e689a60661fa75701
mssuloaga <103213960+mssuloaga@users.noreply.github.com>
2022-07-24 18:39:32 -0400

    Revert "Update index.blade.php"
    This reverts commit 2f2fd58fc9ae6fa0c3880ff22a89cdf7f2c737a2.

--- resources/views/articulos/index.blade.php
+++ resources/views/articulos/index.blade.php
@@ -9,7 +9,9 @@
     </section>
     <section class="container container-fluid">
         <form class="form-inline" action="{{ route('articulos.busqueda') }}">
+
             <input name="buscarpor" class="form-control mr-sm-2" type="search" placeholder="Buscar por nombre" aria-label="Search">
+
             <button class="btn btn-outline-success my-2 my-sm-0 bi bi-search" type="submit"> Buscar</button>
         </form>
     </section>
