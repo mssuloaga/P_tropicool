21e89868d416e31c7c6d09365f5e9b888eb2e771
MatiPino23 <mpino@ing.ucsc.cl>
2022-07-24 21:01:32 -0400

    Validacion de permisos

---
+++ app/Http/Requests/PermisosEditRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PermisosEditRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|unique:permissions|min:3|max:20'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'name.required' => 'El nombre es requerido',
+            'name.unique'=> 'El nombre ya existe',
+        ];
+    }
+}
