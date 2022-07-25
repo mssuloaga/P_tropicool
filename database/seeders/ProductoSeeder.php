33649bd302b15a000df63b7db484fb3954d5de16
mssuloaga <103213960+mssuloaga@users.noreply.github.com>
2022-07-24 22:33:29 -0400

    seeder producto

--- database/seeders/ProductoSeeder.php
+++ database/seeders/ProductoSeeder.php
@@ -20,7 +20,7 @@ class ProductoSeeder extends Seeder
                   'descripcion'  => 'Carcasa transparente de cilicona',
                   'precio'  => '6000',
                   'cantidad'  => '10',
-                  'imagen'  => 'image2.png',
+                  'imagen'  => 'carcasa8.jpg',
                   'id_categorias'  => '1',
                   
               )
@@ -32,7 +32,7 @@ class ProductoSeeder extends Seeder
                   'descripcion'  => 'Carcasa de cilicona roja',
                   'precio'  => '7000',
                   'cantidad'  => '15',
-                  'imagen'  => 'image.png',
+                  'imagen'  => 'carcasa13.jpg',
                   'id_categorias'  => '1',
                   
               )
@@ -44,7 +44,7 @@ class ProductoSeeder extends Seeder
                   'descripcion'  => 'Cargador de 5 volt blanco usb ',
                   'precio'  => '15000',
                   'cantidad'  => '15',
-                  'imagen'  => 'image2.png',
+                  'imagen'  => 'cargador.jpg',
                   'id_categorias'  => '2',
                   
               )
@@ -56,7 +56,7 @@ class ProductoSeeder extends Seeder
                   'descripcion'  => 'Cargador de 14.5 volt blanco usb-c ',
                   'precio'  => '24000',
                   'cantidad'  => '11',
-                  'imagen'  => 'image.png',
+                  'imagen'  => 'cargarapida.jpg',
                   'id_categorias'  => '2',
                   
               )
